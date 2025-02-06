9a6640f13aa9b72c3e48c5e868d556a45052bb2c
Velina Ivanova <velina.ivanova@proton.me>
2025-02-05 22:45:28 -0800

    Add task role to ECS task definition

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -10,6 +10,7 @@ resource "aws_ecs_task_definition" "this" {
   cpu                      = "256"
   memory                   = "512"
   execution_role_arn       = aws_iam_role.ecs_execution.arn
+  task_role_arn            = aws_iam_role.ecs_task.arn
 
   container_definitions = jsonencode([
     {
